561197469152ab5fd47498705c9ead51c7197dcf
DanielRTato <drodrigueztato@danielcastelao.org>
2024-11-12 18:54:53 +0100

    EJ1

---
+++ Boletin_6/src/Ej1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ej1 {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el primer número");
+        int n1 = teclado.nextInt();
+        System.out.println("Introduce el segundo número");
+        int n2 = teclado.nextInt();
+        System.out.println(comprendidos(n1,n2));
+    }
+    public static int comprendidos(int a, int b){
+        for (int i=1;a!=b;a--){
+            System.out.println(a);
+        }
+    return a;
+    }
+}
