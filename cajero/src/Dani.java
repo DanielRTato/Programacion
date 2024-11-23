99fd56f333508478277a7dc475876ed56c020ee8
DanielRTato <drodrigueztato@danielcastelao.org>
2024-11-23 20:33:07 +0100

    1

---
+++ cajero/src/Dani.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Dani {
+
+    public static void sayName1(){
+        System.out.println("My name is Daniel1");
+    }
+    public static void sayAg1e(){
+        System.out.println("Im 27 years old1");
+    }
+
+
+
+}
+
+
---
+++ cajero/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+    sayName();
+    sayAge();
+    Dani.sayName1();
+    Dani.sayAg1e();
+    }
+
+    public static void sayName(){
+        System.out.println("My name is Daniel");
+    }
+    public static void sayAge(){
+        System.out.println("Im 27 years old");
+    }
+}
