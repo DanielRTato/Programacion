2c55d63883c7a043d4b51bd42556985e43de52b3
DanielRTato <drodrigueztato@danielcastelao.org>
2024-11-30 14:01:32 +0100

    .

--- encuentraDuplicados/src/Main.java
+++ encuentraDuplicados/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         String frase= "Cuantos duplicados tiene esta frase";
+        String letra= "";
+        String duplicados= "";
 
         for (int i=0; i<frase.length(); i++){
             System.out.println(frase.charAt(i));
