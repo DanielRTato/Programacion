c0438a94ebf6489673c4163fa6cb7ba30ecc8150
DanielRTato <drodrigueztato@danielcastelao.org>
2024-12-02 19:12:02 +0100

    cantidad de numeros negativos, positos y ceros que tiene un array.

---
+++ Boletin_5/src/Ej2.java
@@ -0,0 +1,10 @@
+/**
+ * Un programa que calcule a suma e o produto dos números comprendidos entre 10 e 90.
+ * @author Daniel Rodríguez Tato
+ * @version 1.0
+ */
+public class Ej2 {
+    public static void main(String[] args) {
+
+    }
+}
