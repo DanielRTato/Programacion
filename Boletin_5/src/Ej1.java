fb93950cecd233a04c2ca7597a9479d41fc03db4
DanielRTato <drodrigueztato@danielcastelao.org>
2024-12-02 19:08:45 +0100

    cantidad de numeros negativos, positos y ceros que tiene un array.

--- Boletin_5/src/Ej1.java
+++ Boletin_5/src/Ej1.java
@@ -1,6 +1,34 @@
+/**
+ * Calcula a cantidade de números negativos, positivos e ceros que hai nun grupo de 10 números enteiros.
+ * @ Daniel Rodríguez Tato
+ * @version 1.0
+ */
 public class Ej1 {
     public static void main(String[] args) {
 
+        int [] numeros;
+        int positivos = 0; // Contador de números positivos
+        int negativos = 0;  // Contador de números negativos
+        int ceros = 0;      // Contador de ceros
+        numeros = new int[]{0,0,1,5,-1,-7,34,0,-134,0}; //Array que almacena los numeros
+
+        // Recorre el array y cuenta cada tipo de número
+        for (int numero : numeros) {
+            if (numero > 0) {
+                positivos++;
+            }
+            else if (numero == 0) {
+                ceros++;
+            }
+            else  {
+                negativos++;
+            }
+        }
+
+        // Imprimir los resltados
+        System.out.println("La cantidad de numeros negativos es: " +negativos);
+        System.out.println("La cantidad de numeros positovs es: " +positivos);
+        System.out.println("La cantidad de ceros es: " +ceros);
 
     }
 }
