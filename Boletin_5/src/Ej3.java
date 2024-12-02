eb3b4ae77508ad63479f60529ca65e2614dd9864
DanielRTato <drodrigueztato@danielcastelao.org>
2024-12-02 19:33:07 +0100

    start

---
+++ Boletin_5/src/Ej3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+/**
+ *  Deseña un programa que calcule o área dun rectángulo
+ *  cuxa base e altura pides por teclado. Asegúrate que estes valores sexan positivos,
+ *  para eso valida os datos.
+ * @author Daniel Rodríguez Tato
+ * @version 1.0
+ */
+public class Ej3 {
+    public static void main(String[] args) {
+
+    }
+}
