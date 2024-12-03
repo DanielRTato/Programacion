34fd408455db29bcaf939822895aeaf25fb60a2d
DanielRTato <drodrigueztato@danielcastelao.org>
2024-12-03 19:07:06 +0100

    pendiente de comentar y revisar

--- Boletin_5/src/Ej8.java
+++ Boletin_5/src/Ej8.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * programa que solicite 10 números por consola e calcule a súa suma.
  * Si o usuario introduce en  999,
@@ -6,5 +7,20 @@
 public class Ej8 {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        int contador = 0;
+        int resultado = 0;
+
+        do {
+            System.out.println("Introduce un numero");
+            int numero = scan.nextInt();
+            contador++;
+            resultado +=numero;
+            if (numero==999) break;
+        }
+        while (contador<10);
+
+        System.out.println("El resultado de la suma es: " +resultado);
     }
 }
