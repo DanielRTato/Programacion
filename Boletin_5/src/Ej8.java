c3f8dc9c1c906b71527b5f092fa3249ca1367968
DanielRTato <drodrigueztato@danielcastelao.org>
2024-12-03 18:59:08 +0100

    pendiente de comentar

---
+++ Boletin_5/src/Ej8.java
@@ -0,0 +1,10 @@
+/**
+ * programa que solicite 10 números por consola e calcule a súa suma.
+ * Si o usuario introduce en  999,
+ * deixa de solicitar máis números
+ */
+public class Ej8 {
+    public static void main(String[] args) {
+
+    }
+}
