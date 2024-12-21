3181db2c1c317ea27ee9b2cd24067ef50d9e1b7d
DanielRTato <drodrigueztato@danielcastelao.org>
2024-12-22 00:47:35 +0100

    Ej 1 terminado

---
+++ Boletin_8/src/Ej1.java
@@ -0,0 +1,11 @@
+/**
+ * Muestra la longitud de un String
+ */
+public class Ej1 {
+    public static void main(String[] args) {
+
+        String frase = "Isto é Java!";
+        System.out.println(frase.length());
+
+    }
+}
