293f0feeecaa4c40312d3455c309941e5962e28a
DanielRTato <drodrigueztato@danielcastelao.org>
2024-12-22 00:53:50 +0100

    Ej 2 terminado

---
+++ Boletin_8/src/Ej2.java
@@ -0,0 +1,13 @@
+/**
+ * Muestra el String letra a letra
+ */
+public class Ej2 {
+    public static void main(String[] args) {
+
+        String str = "Java";
+
+        for (int i=0; i<str.length(); i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
