50d727bb5be8d41374173e7bce57a0848ff59999
DanielRTato <drodrigueztato@danielcastelao.org>
2024-12-22 01:26:55 +0100

    Ej 4 terminado

---
+++ Boletin_8/src/Ej4.java
@@ -0,0 +1,24 @@
+/**
+ * Imprime un texto sin espacios de dos formas
+ */
+public class Ej4 {
+    public static void main(String[] args) {
+
+        String str = "James Gosling Created Java";
+        char espacio = ' ';
+
+        // Primer metodo
+        for (int i = 0; i<str.length(); i++){
+            if (str.charAt(i) != espacio){
+                System.out.println(str.charAt(i));
+            }
+        }
+
+        // Segundo metodo
+         String sinEspacio = str.replace(" ", "");
+        System.out.println(sinEspacio);
+
+
+
+    }
+}
