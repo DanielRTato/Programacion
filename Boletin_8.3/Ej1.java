54cf97cba590521a293301640a217eae73fc874a
Daniel RTato <drodrigueztato@danielcastelao.org>
2025-01-27 13:31:07 +0100

    terminado//sin comentar

--- Boletin_8.3/Ej1.java
+++ Boletin_8.3/Ej1.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+
 public class Ej1 {
     public static void main(String[] args) {
-        System.out.println("Helhgghhjghjlo world!");
+
+        ArrayList<String> lista = new ArrayList<>();
+        desenvolver("feigorificos",4,lista);
+
+        for (String palabra:lista) {
+            System.out.println(palabra);
+        }
+
+
     }
+    public static void desenvolver(String palabra, int n,ArrayList<String> lista){
+
+        for (int i=0; i<=palabra.length()-n;i++){
+            String str= palabra.substring(i,n+i);
+            lista.add(str);
+
+
+
+        }
+
+    }
+
 }
