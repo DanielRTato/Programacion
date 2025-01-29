ca13a0c8083dddcd420cb5566bd4de2df2ca065c
DanielRTato <drodrigueztato@danielcastelao.org>
2025-01-29 20:01:10 +0100

    terminado

---
+++ Boletin_8/src/Ej8.java
@@ -0,0 +1,19 @@
+/**
+ * Compara dos Strings
+ */
+public class Ej8 {
+    public static void main(String[] args) {
+
+        String texto1 = "Java";
+        String texto2= "JavaScript";
+        boolean comparacion;
+
+      comparacion= texto1.equalsIgnoreCase(texto2);
+
+      if (comparacion==true){
+          System.out.println("Son iguales");
+      }else{
+          System.out.println("Son distintos");
+      }
+    }
+}
