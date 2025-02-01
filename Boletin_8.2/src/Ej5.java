d0a99d366d62aeb7d6acea2d378ff9b8ecb89e5f
DanielRTato <drodrigueztato@danielcastelao.org>
2025-02-01 23:56:38 +0100

    fin//pendiente comentar

--- Boletin_8.2/src/Ej5.java
+++ Boletin_8.2/src/Ej5.java
@@ -1,4 +1,34 @@
 package src;
 
 public class Ej5 {
+    public static void main(String[] args) {
+
+        String texto = "universal serial bus";
+
+        siglas(texto);
+
+
+    }
+
+    /**
+     * Imprime las siglas
+     * @param texto
+     */
+    public static void  siglas (String texto){
+        StringBuilder siglas = new StringBuilder();
+        siglas.append(texto.charAt(0));
+        for (int i =0; i<texto.length();i++){
+            char c = texto.charAt(i);
+            if (Character.isWhitespace(c)){
+                i++;
+                siglas.append(texto.charAt(i));
+            }
+        }
+        System.out.println(siglas.toString().toUpperCase());
+    }
+
+    public static void mayuscula (String texto){
+
+    }
+
 }
