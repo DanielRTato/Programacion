1640affd6c7248450567b09d024ff91741689fc2
DanielRTato <drodrigueztato@danielcastelao.org>
2025-02-01 12:18:15 +0100

    fin//pendiente comentar

---
+++ Boletin_8/src/Ej9.java
@@ -0,0 +1,21 @@
+/**
+ * Sustituye vocales
+ */
+public class Ej9 {
+    public static void main(String[] args) {
+
+        String texto = " Jeve jeve jeve";
+
+        System.out.println("El texto original :" + texto);
+
+        cambiar(texto);
+
+
+    }
+    public static void cambiar (String texto){
+        String textoa = texto.replace('e','a');
+
+        System.out.println("El texto modificado : " + textoa);
+
+    }
+}
