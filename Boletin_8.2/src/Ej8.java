55305b5942d1b17320cdd40ebbddc3b98136aec1
DanielRTato <drodrigueztato@danielcastelao.org>
2025-02-02 19:21:48 +0100

    pendiente

---
+++ Boletin_8.2/src/Ej8.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Ej8 {
+    public static void main(String[] args) {
+
+        String binario ="1010";
+    }
+
+    public static String decimal (String binario){
+        String decimal = binario.getBytes(binario);
+    }
+}
