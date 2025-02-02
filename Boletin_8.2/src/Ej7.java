2ce51d3a308b870ce984bc57162e102baeb76dc6
DanielRTato <drodrigueztato@danielcastelao.org>
2025-02-02 19:04:55 +0100

    fin//pendiente comentar

--- Boletin_8.2/src/Ej7.java
+++ Boletin_8.2/src/Ej7.java
@@ -3,6 +3,40 @@ package src;
 public class Ej7 {
     public static void main(String[] args) {
 
+        String cadea = "cadea";
+        String subcadea = "subcadea";
+        String texto = "eee";
+        String texto1 = "aaa";
+
+        System.out.println(compruebaCadenas(cadea,subcadea)? "Es subcadena" : "No es subcadena");
+        System.out.println("La primera es " + ordenAlfabetico(texto,texto1));
 
     }
+
+    /**
+     * Comprueba si un String esta dentro de otro
+     * @param cadea
+     * @param subcadea
+     * @return true si pertece a otro String o false si no
+     */
+    public static boolean compruebaCadenas ( String cadea, String subcadea){
+        return subcadea.contains(cadea);
+    }
+
+    /**
+     * Devuelve el primer String
+     * @param texto
+     * @param texto1
+     * @return
+     */
+    public static String ordenAlfabetico (String texto, String texto1){
+
+        int compara = texto.compareTo(texto1);
+        if (compara<0){
+            return texto;
+        }else {
+            return texto1;
+        }
+    }
 }
+
