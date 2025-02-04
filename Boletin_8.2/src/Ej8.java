2125ca34f2675db97eed5bfa3dadc68dab92aae8
DanielRTato <drodrigueztato@danielcastelao.org>
2025-02-04 18:48:24 +0100

    terminado/sin comentar

--- Boletin_8.2/src/Ej8.java
+++ Boletin_8.2/src/Ej8.java
@@ -1,12 +1,24 @@
 package src;
 
+/**
+ * pasa de binario a decimal
+ */
 public class Ej8 {
     public static void main(String[] args) {
 
-        String binario ="1010";
+        String binario="1010";
+        System.out.println(binarioADecimal(binario));
     }
-
-    public static String decimal (String binario){
-       // String decimal = binario.getBytes(binario);
+    public static int binarioADecimal(String binario){
+        int decimal=0;
+        for(int i=0;i<binario.length();i++){
+            if(binario.charAt(i)=='1'){
+                decimal=decimal+(int)Math.pow(2,binario.length()-1-i);
+            }
+            else if(binario.charAt(i)!='0'){
+                return -1;
+            }
+        }
+        return decimal;
     }
 }
