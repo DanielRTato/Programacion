631c99112796c477728d4965159c4512d2490c78
Daniel RTato <drodrigueztato@danielcastelao.org>
2025-02-12 10:34:06 +0100

    pendiente mostrar data y dataigual(), incrementaAno

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+    Calendario cal = new Calendario(5,7,10);
+    cal.setYear();
+    cal.getYear();
+    cal.setDay();
+    cal.getDay();
+    cal.setMonth();
+    cal.getMonth();
+    cal.incrementaMes();
+    cal.incrementarDia();
+
 
 
     }
