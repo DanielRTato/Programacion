061ef401127d8af778d6a14413959bb1a96ca2a8
DanielRTato <drodrigueztato@danielcastelao.org>
2025-03-14 19:43:40 +0100

    agregar constructor sobrecargado en Coche para permitir instanciación con año por defecto

--- PracticaPOO/src/Main.java
+++ PracticaPOO/src/Main.java
@@ -2,11 +2,12 @@ import orientacionobjetos.Coche;
 
 public class Main {
     public static void main(String[] args) {
+
         // instanciar un objeto de la clase Coche
         Coche coche1 = new Coche("Toyota", "Corolla", 2021);
         coche1.arrancar();
 
-        Coche coche2 = new Coche("Nissan", "Sentra", 2020);
+        Coche coche2 = new Coche("Nissan", "Sentra");
         coche2.arrancar();
     }
 }
--- PracticaPOO/src/orientacionobjetos/Coche.java
+++ PracticaPOO/src/orientacionobjetos/Coche.java
@@ -1,17 +1,31 @@
 package orientacionobjetos;
 
-public class Coche {
+public class Coche {  //el nombre de las clases va en mayúscula
+
     // atributos o propiedades
     private String marca;
     private String modelo;
     private int anio;
 
-    // constructor
+    // constructor puede ser vacío o con parámetros (atributos), puedes tener ceros o más constructores
+    // tanto el constructor como los getters y setters se pueden generar automáticamente en IntelliJ
     public Coche(String marca, String modelo, int anio) {
         this.marca = marca;
         this.modelo = modelo;
         this.anio = anio;
     }
+
+    /*public Coche (String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anio = 2020;
+    }*/
+
+    // constructor con this para reutilizar código
+    public Coche (String marca, String modelo){
+        this(marca, modelo, 2020);
+    }
+
 
     public void arrancar() {
         System.out.println("El coche %s %s %d ha arrancado".formatted(marca, modelo, anio));
