c481f4cb06b6218c8b7a406ad6b80faae2d58401
DanielRTato <drodrigueztato@danielcastelao.org>
2025-03-14 21:36:35 +0100

    añadir atributos

--- PracticaPOO/src/Main.java
+++ PracticaPOO/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // instanciar un objeto de la clase Coche
-        Coche coche1 = new Coche("Toyota", "Corolla", 2021);
+        Coche coche1 = new Coche("Toyota", "Corolla", 2021,1,1);
         coche1.arrancar();
 
         Coche coche2 = new Coche("Nissan", "Sentra");
--- PracticaPOO/src/orientacionobjetos/Coche.java
+++ PracticaPOO/src/orientacionobjetos/Coche.java
@@ -6,13 +6,17 @@ public class Coche {  //el nombre de las clases va en mayúscula
     private String marca;
     private String modelo;
     private int anio;
+    private double velocidadActual;
+    private double litrosEnDeposito;
 
     // constructor puede ser vacío o con parámetros (atributos), puedes tener ceros o más constructores
     // tanto el constructor como los getters y setters se pueden generar automáticamente en IntelliJ
-    public Coche(String marca, String modelo, int anio) {
+    public Coche(String marca, String modelo, int anio,double velocidadActual, double litrosEnDeposito) {
         this.marca = marca;
         this.modelo = modelo;
         this.anio = anio;
+        this.velocidadActual = velocidadActual;
+        this.litrosEnDeposito = litrosEnDeposito;
     }
 
     /*public Coche (String marca, String modelo) {
@@ -23,10 +27,10 @@ public class Coche {  //el nombre de las clases va en mayúscula
 
     // constructor con this para reutilizar código
     public Coche (String marca, String modelo){
-        this(marca, modelo, 2020);
+        this(marca, modelo, 2020,0,0);
     }
 
-
+    // metodo o comportamiento
     public void arrancar() {
         System.out.println("El coche %s %s %d ha arrancado".formatted(marca, modelo, anio));
     }
