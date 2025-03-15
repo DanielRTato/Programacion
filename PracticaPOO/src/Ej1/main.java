2a18331b891deb04ad7933c6798217229d2f942d
DanielRTato <drodrigueztato@danielcastelao.org>
2025-03-15 12:40:08 +0100

    añadir instancias de la clase Alumno y métodos de comparación

--- PracticaPOO/src/Ej1/main.java
+++ PracticaPOO/src/Ej1/main.java
@@ -11,6 +11,25 @@ public class main {
         System.out.println(alumno2.toString());
         System.out.println(alumno2.esMayorDeEdad());
 
+        Alumno alumno3 = new Alumno();
+        alumno3.setNombre("Alex");
+        alumno3.setApellido("Gomez");
+        alumno3.setEmail("asdasd@asd");
+        alumno3.setEdad(20);
+        alumno3.setTelefono(986111111);
+        System.out.println(alumno3); //toString implicito
+
+        Alumno alumno4 = new Alumno("Juan", "Perez",11);
+        alumno4.setTelefono(986111111);
+        alumno4.setEmail("asdasd@asd");
+        System.out.println(alumno4); //toString implicito
+        System.out.println("El alumno %s %s %s es mayor de edad".formatted(alumno4.getNombre(), alumno4.getApellido(), alumno4.esMayorDeEdad() ? "si" : "no"));
+
+        if (alumno1.equals(alumno2)) {
+            System.out.println("Los alumnos son iguales");
+        }else{
+            System.out.println("No son el mismo alumno");
+        }
 
 
 
