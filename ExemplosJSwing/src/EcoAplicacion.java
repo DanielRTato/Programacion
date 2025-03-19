7f22f7ef958c2e23a7edc599d90fd815ab716022
DanielRTato <drodrigueztato@danielcastelao.org>
2025-03-19 10:34:16 +0100

    Implement EcoAplicacion class with basic Swing components and main application entry point

--- ExemplosJSwing/src/EcoAplicacion.java
+++ ExemplosJSwing/src/EcoAplicacion.java
@@ -1,7 +1,37 @@
 import javax.swing.*;
 
 public class EcoAplicacion extends JFrame {
-
     private JTextField cadroTexto;
+
+    public EcoAplicacion(){
+        super();
+        setTitle("Primera aplicacion Swing"); // Titulo de la ventana
+
+        JComponent botoneria = creaBotones(); // Crea la botoneria
+        JComponent panelCadtroTexto = creaPanelCadro();
+
+        JSplitPane panelPrincipal = new JSplitPane(JSplitPane.VERTICAL_SPLIT, botoneria, panelCadtroTexto); // Crea un panel dividido
+        getContentPane().add(panelPrincipal); // Añade el panel dividido a la ventana
+        pack(); // Ajusta el tamaño de la ventana
+        setVisible(true); // Hace visible la ventana por defecto es invisible
+    }
+
+    private JComponent creaBotones(){
+        JPanel botoneria = new JPanel(); // Crea un panel
+        JButton btnHola = new JButton("Di Hola"); // Crea un boton
+
+        botoneria.add(btnHola); // Añade el boton al panel
+
+        return botoneria;
+    }
+
+    private JComponent creaPanelCadro(){
+        // no uso un JPanel porque no necesito añadir más componentes
+        JTextField txtCadro = new JTextField(); // Crea un cuadro de texto
+
+        return txtCadro;
+    }
+
+
 
 }
