cbcf0c3a978962a80cf1da5b753c5fdd50dd14cf
DanielRTato <drodrigueztato@danielcastelao.org>
2025-03-31 13:25:48 +0200

    comentar código y añadir ejemplo de excepciones

--- POO/src/exceptions/ExemploExcemptions.java
+++ POO/src/exceptions/ExemploExcemptions.java
@@ -9,9 +9,14 @@ public class ExemploExcemptions {
 
         try {
             c = a / b;
-        } catch (ArithmeticException erro) {
+        } catch (ArithmeticException | IllegalArgumentException erro) {
             System.out.println("Intentalo de nuevo");
             System.out.println(erro.getMessage());
+        }//catch (Exception erro2){
+         //   System.out.println(erro2.getMessage());
+        //}
+        finally {
+            System.out.println("Siempre se ejecuta");
         }
 
     }
