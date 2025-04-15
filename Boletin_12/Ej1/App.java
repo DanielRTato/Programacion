b7f1810aec67f85bbebcab598ee608932e935566
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-16 01:54:23 +0200

    Ejemplos de trabajar con archivos

--- Boletin_12/Ej1/App.java
+++ Boletin_12/Ej1/App.java
@@ -1,4 +1,30 @@
 package Ej1;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("1- ");
+            System.out.println("2- ");
+            System.out.println("3- ");
+            System.out.println("4- ");
+            System.out.println("0- Salir");
+            opcion = teclado.nextInt();
+
+            switch (opcion){
+                case 1 ->
+                case 2 ->
+                case 3 ->
+                case 4 ->
+                default -> System.out.println("Opcion no valida");
+            }
+        } while (opcion != 0);
+
+
+    }
 }
--- Boletin_12/Ej1/Cliente.java
+++ Boletin_12/Ej1/Cliente.java
@@ -3,11 +3,40 @@ package Ej1;
 public class Cliente {
     private String id;
     private String nome;
-    private String telefono;
+    private int telefono;
 
-    public Cliente (String id, String nome, String telefono) {
+    public Cliente (String id, String nome, int telefono) {
         this.id = id;
         this.nome = nome;
         this.telefono = telefono;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void listarClientes(){
+
+    }
+
 }
