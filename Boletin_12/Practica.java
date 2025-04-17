f761b1a1a2b1facc68faa38b80c68618bdd86ffc
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-18 01:46:46 +0200

    clase fileReader

--- Boletin_12/Practica.java
+++ Boletin_12/Practica.java
@@ -1,9 +1,12 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Practica {
     public static void main(String[] args) {
 
+        leerFichero accediento = new leerFichero();
+        accediento.lee();
 
     }
 }
@@ -13,7 +16,15 @@ class leerFichero {
     public void lee (){
         try {
             FileReader entrada = new FileReader("nombre.txt");
-        } catch (FileNotFoundException e) {
+
+            int c = entrada.read();
+
+            while (c != -1) {
+                c = entrada.read();
+                char letra = (char) c;
+                System.out.print(letra);
+            }
+        } catch ( IOException e) {
             System.out.println("Archivo no encontrado");
         }
     }
