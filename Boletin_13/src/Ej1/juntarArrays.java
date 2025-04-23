382c48df9290ce193507cf040042e59b6b5f603f
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-23 20:35:52 +0200

    juntarArrays()

---
+++ Boletin_13/src/Ej1/juntarArrays.java
@@ -0,0 +1,24 @@
+package Ej1;
+
+import java.util.Arrays;
+
+public class juntarArrays <T> {
+
+    private T[] array1;
+    private  T[] array2;
+
+    public juntarArrays(T[] array1, T[] array2) {
+        this.array1 = array1;
+        this.array2 = array2;
+    }
+
+    public T[] concatenarManual() {
+        T[] resultado = Arrays.copyOf(array1, array1.length + array2.length);
+        for (int i = 0; i < array2.length; i++) {
+            resultado[array1.length + i] = array2[i];
+        }
+        return resultado;
+    }
+
+    }
+
