d7b042739969f8d0c71c55f49efd329fa43d1c6f
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-26 23:58:21 +0200

    ejemplos de uso de la clase File

--- Ficheros/src/Main.java
+++ Ficheros/src/Main.java
@@ -5,9 +5,11 @@ public class Main {
 
         File fichero = new File("ejemplo1.txt");
 
-        if (fichero.exists()) System.out.println("El fichero existe");
-        else System.out.println("El fichero no existe");
-
+        if (fichero.exists()) {
+            System.out.println("El fichero existeee");
+        }else {
+            System.out.println("El fichero no existe");
+        }
         System.out.println("Nombre: " + fichero.getName());
         System.out.println("Longitud: " + fichero.length());
         System.out.println("Ruta absoluta: "+ fichero.getAbsolutePath());
