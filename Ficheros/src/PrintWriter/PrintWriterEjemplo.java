e9d4a7b0ca08a65a7f001539bc131e2e26d88e74
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-29 20:39:44 +0200

    ejemplo de PrintWrier

---
+++ Ficheros/src/PrintWriter/PrintWriterEjemplo.java
@@ -0,0 +1,28 @@
+package PrintWriter;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class PrintWriterEjemplo {
+    public static void main(String[] args) {
+
+        String fichero = "ejemplo.txt";
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter(fichero));
+            pw.print("Esto es un texto sin salto de línea");
+            pw.println("Nueva palabra");
+            pw.println("Esto es un texto con salto de línea");
+            pw.println(4.5455);
+
+            Arrays.stream(new int[] {1,2,3,4,10})
+                    .filter(n -> n > 2)
+                    .map(n -> n * 2)
+                    .forEach(n -> pw.println(n));
+            pw.close();
+        }catch (FileNotFoundException e){
+            System.out.println("Fichero no encontrado");
+        }catch (IOException e){
+            System.out.println("Problemas al escribir en el fichero");
+        }
+    }
+}
