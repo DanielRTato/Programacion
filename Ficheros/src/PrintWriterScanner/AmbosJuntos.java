684b0888bf8327605afe30a9b7fbb0438c696cdf
DanielRTato <drodrigueztato@danielcastelao.org>
2025-04-29 21:13:27 +0200

    ejemplo de Scanner fix

--- Ficheros/src/PrintWriterScanner/AmbosJuntos.java
+++ Ficheros/src/PrintWriterScanner/AmbosJuntos.java
@@ -2,6 +2,7 @@ package PrintWriterScanner;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -12,11 +13,12 @@ public class AmbosJuntos {
             String nombreFichero = "ficherosEnteros.txt";
             PrintWriter pwr = new PrintWriter(nombreFichero);
 
-            for (int i = 1; i <=1000; i++) {
+            for (int i = 1; i <=1000; i ++) {
                 pwr.print(i + " ");
                 if ( i % 100 == 0){
                     pwr.println();
                 }
+            }
                 pwr.close();
 
                 Scanner scn = new Scanner(new FileReader(nombreFichero));
@@ -24,8 +26,8 @@ public class AmbosJuntos {
                     System.out.println("Valor leído: " + scn.nextInt());
 
                 }
-            }
-        }catch (FileNotFoundException e) {
+
+        }catch (IOException e) {
             System.out.println("Archivo no encontrado");
         }
     }
