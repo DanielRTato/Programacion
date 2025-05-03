d2a8c4880a05726688c53900b75005c436d8113f
DanielRTato <drodrigueztato@danielcastelao.org>
2025-05-03 18:31:30 +0200

    Enunciado

--- Ficheros/src/Ejercicios/crearArchivos.java
+++ Ficheros/src/Ejercicios/crearArchivos.java
@@ -1,4 +1,8 @@
 package Ejercicios;
 
+/**
+ * Crea n archivos, nombre(n).txt en la carpeta que el usuario elija
+ * Dentro de cada archivo escribe la frase "Este es el fichero nombre(n).txt"
+ */
 public class crearArchivos {
 }
