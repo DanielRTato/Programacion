3d435e26a824edf6b68a07536a8bed05a53cabb9
DanielRTato <drodrigueztato@danielcastelao.org>
2025-05-03 23:28:40 +0200

    Enunciado

--- Ficheros/src/Ejercicios/crearArchivos.java
+++ Ficheros/src/Ejercicios/crearArchivos.java
@@ -1,8 +1,30 @@
 package Ejercicios;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Crea n archivos, nombre(n).txt en la carpeta que el usuario elija
  * Dentro de cada archivo escribe la frase "Este es el fichero nombre(n).txt"
  */
 public class crearArchivos {
+    public static void main(String[] args) {
+
+        creaArchivos("C:\\Pro3\\f\\carpeta",5);
+    }
+
+    public static void creaArchivos(String ruta, int cantidad) {
+        for (int i  = 1; i <= cantidad; i++) {
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(ruta +"nombree" + i + ".txt"));
+                writer.write("Este es e fichero nombre" + i + ".txt");
+            } catch (IOException e) {
+                System.out.println("Error al crear el fichero");
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
