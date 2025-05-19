05840bcc617fb8c75011b2d4448d44dd7595695e
DanielRTato <drodrigueztato@danielcastelao.org>
2025-05-19 21:17:48 +0200

    Ejemplo de uso List y arrayList

--- ColeccionesOW/src/colecciones/EjemploList.java
+++ ColeccionesOW/src/colecciones/EjemploList.java
@@ -2,6 +2,7 @@ package colecciones;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class EjemploList {
@@ -24,14 +25,38 @@ public class EjemploList {
         System.out.println(listaProductos);
 
         // Ejemplo de ArrayList
-        List<Integer> lista3 = new ArrayList<>();
-        lista3.add(10);
+        List<Integer> lista3 = new ArrayList<>(); // Creamos una lista vacía
+        lista3.add(10); // Añadimos elementos a la lista
         lista3.add(20);
-        lista3.addAll(List.of(1,2,3,4,5));
+        lista3.addAll(List.of(1,2,3,4,5)); // Añadimos una lista a la lista como un solo elemento
         lista3.add(6);
-        System.out.println(lista3);
+        System.out.println(lista3); // Imprimimos la lista
 
+        // Recorremos la lista con un bucle for-each
+        for(int n : lista3) {
+            System.out.println(n); // Imprimimos cada elemento de la lista
+        }
 
+        for(Iterator<Integer> it = lista3.iterator(); it.hasNext(); ) {
+            int n = it.next();
+           if (n %10 == 0 || n == 5) {
+               it.remove();
+           }
+        }
+        System.out.println(lista3); // Imprimimos la lista después de remover los elementos que cumplen la condición
+
+        // Ejemplos de buscar la posición de un elemento en la lista
+        int pos = lista3.indexOf(2); // Buscamos la posición del elemento 5 en la lista si no lo encuentra, devuelve -1
+        System.out.println("La posicion: " + pos);
+        lista3.addAll(Arrays.asList(5,6,7,8,9,1));
+        pos = lista3.lastIndexOf(1); // Buscamos la última posición del elemento 1 en la lista si no lo encuentra, devuelve -1
+        System.out.println("La ultima posicion: " + pos);
+
+        // Ejempos de cotener
+        boolean contains = lista3.contains(5); // Comprobamos si la lista contiene el elemento 5
+        System.out.println(contains);
+        contains = lista3.containsAll(List.of(5,6,7)); // Comprobamos si la lista contiene todos los elementos de la lista de entrada
+        System.out.println(contains);
 
 
     }
