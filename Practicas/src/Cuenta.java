4c40a7c139ec7378cf7c73109ffab5a71f77c8c5
DanielRTato <drodrigueztato@danielcastelao.org>
2025-06-01 23:38:07 +0200

    Creando clases

--- Practicas/src/Cuenta.java
+++ Practicas/src/Cuenta.java
@@ -1,2 +1,37 @@
 public class Cuenta {
+
+    double balance;
+
+    public Cuenta(double balance) {
+        this.balance = balance;
+    }
+
+    public  void mostrarBalance() {
+        System.out.println("Tu balance actual es: " + getBalance());
+    }
+
+    public void depositar (double cantidad) {
+        if (cantidad > 0 ) {
+            balance += cantidad;
+        } else {
+            System.out.println("La cantidad introducida no es valida");
+        }
+    }
+
+    public void retirar(double cantidad) {
+        if (cantidad > 0) {
+            balance -= cantidad;
+        } else {
+            System.out.println("La cantidad introducida no es valida");
+
+        }
+    }
+
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+
 }
--- Practicas/src/Menu.java
+++ Practicas/src/Menu.java
@@ -1,7 +1,46 @@
 import java.util.Scanner;
 
 public class Menu {
-    Scanner teclado = new Scanner(System.in)
-    public static void Menu () {
+
+
+    public static void Menu() {
+        boolean isRunning = true;
+        Scanner teclado = new Scanner(System.in);
+        int opcion;
+        double cantidad = 0;
+        Cuenta cuenta = new Cuenta(0);
+
+        while (isRunning) {
+            System.out.println("PROGRAMA BANCARIO");
+            System.out.println("--------------------");
+            System.out.println("1. Balance");
+            System.out.println("2. Depositar");
+            System.out.println("3. Sacar");
+            System.out.println("4. Salir");
+            System.out.println("--------------------");
+            System.out.println("Elige una opcion (1 al 4)");
+            opcion = teclado.nextInt();
+
+            switch (opcion) {
+                case 1 -> cuenta.mostrarBalance();
+                case 2 -> {
+                    System.out.println("Cuanto deseas depositar?");
+                        cantidad = teclado.nextDouble();
+                    cuenta.depositar(cantidad);
+                }
+                case 3 -> {
+                    System.out.println("Cuanto deseas retirar?");
+                    cantidad = teclado.nextDouble();
+                    cuenta.retirar(cantidad);
+                }
+                case 4 -> {
+                    System.out.println("Saliendo... ");
+                    isRunning = false;
+                }
+                default -> System.out.println("Opcion no valida");
+            }
+
+        }
+        teclado.close();
     }
 }
